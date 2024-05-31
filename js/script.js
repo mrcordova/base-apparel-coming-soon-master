512f27ec14e45e64ee80c8a4ef976d56cb0659d7
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-31 16:14:55 -0700

    create code for input email validation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 const emailInput = document.querySelector('input[type=email]');
+const submitBtn = document.querySelector('button[type=submit]');
+const emailDiv = document.querySelector('.email');
+const errorMessageSpan = document.querySelector('.error');
+let clicked = false;
 
 
 
 emailInput.addEventListener("input", (e) => {
-    console.log(e.currentTarget.value);
+    
+    if (clicked) {
+        e.currentTarget.classList.toggle("email-error", !emailInput.checkValidity() );
+        emailDiv.classList.toggle("email-error-border", !emailInput.checkValidity());
+        errorMessageSpan.classList.toggle('error-hide', emailInput.checkValidity());
+        
+    }
+})
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    clicked = true;
+
+    // console.log(emailInput.checkValidity())
+    emailInput.classList.toggle("email-error", !emailInput.checkValidity() || emailInput.value == "");
+    emailDiv.classList.toggle("email-error-border", !emailInput.checkValidity()|| emailInput.value == "");
+    errorMessageSpan.classList.toggle('error-hide', emailInput.checkValidity() || emailInput.value == "");
+
 })
